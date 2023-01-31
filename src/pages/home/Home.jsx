f04905b307f37d3bff5957222a9b06016a55a325
Patrick-Ngige <wakemanjajr@gmail.com>
2023-01-31 16:04:39 +0300

    creating the home page header

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../components/navbar/Navbar';
+import './home.css';
+import Header from '../../components/header/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+       <Header/>
+    </div>
+  )
+}
+
+export default Home
